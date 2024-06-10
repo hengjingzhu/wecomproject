3e537bdb3c5db022f4fda220b567b1e739448d8a
davidzhu <david3311041@hotmail.com>
2024-06-11 04:18:23 +0800

    日志信息

--- wecom/production_settings.py
+++ wecom/production_settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# 静态文件在容器内的目录
+STATIC_ROOT = '/djangostatic'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
